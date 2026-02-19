29bf2ffd646a58389e7df9b28eaf7c8268a5c99d
Chotika Bangboon <129026858+Chotikabb@users.noreply.github.com>
2026-02-19 10:51:30 +0700

    update sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,38 +1,10 @@
 import { MetadataRoute } from 'next'
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://portfolio-chotika-bangboon.vercel.app/' 
-  
   return [
     {
-      url: baseUrl,
+      url: 'https://portfolio-chotika-bangboon.vercel.app',
       lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 1,
-    },
-    {
-      url: `${baseUrl}/#about`,
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.8,
-    },
-    {
-      url: `${baseUrl}/#experience`,
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.8,
-    },
-    {
-      url: `${baseUrl}/#projects`,
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.7,
-    },
-    {
-      url: `${baseUrl}/#contact`,
-      lastModified: new Date(),
-      changeFrequency: 'monthly',
-      priority: 0.7,
     },
   ]
 }

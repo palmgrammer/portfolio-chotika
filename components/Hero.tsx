a5c69ae9fc08478d35242440df4b1103ee151c03
PalmChoti <chotikabangboon2003@gmail.com>
2026-02-23 14:28:00 +0700

    Update Resume link (add data analyst new version)

--- components/Hero.tsx
+++ components/Hero.tsx
@@ -59,7 +59,8 @@ export default function Hero() {
             </p>
 
             <motion.a
-              href="https://drive.google.com/file/d/1RwCLKZJoxyRK09WZElDAXmsTS00xMLRf/view?usp=sharing"
+              {/* href="https://drive.google.com/file/d/1RwCLKZJoxyRK09WZElDAXmsTS00xMLRf/view?usp=sharing" */}
+              href="https://drive.google.com/file/d/1RVv2yAJ3yN56QKQJ8_vFRUB7vC_lf_kn/view?usp=sharing"
               target="_blank"
               rel="noopener noreferrer"
               whileHover={{ scale: 1.05 }}

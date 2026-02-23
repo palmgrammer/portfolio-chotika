9ada0b70e8e592d310d028c20d31b56549febe75
Chotika Bangboon <129026858+Chotikabb@users.noreply.github.com>
2026-02-23 21:24:58 +0700

    Update layout.tsx for SEO

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,16 +2,36 @@ import type { Metadata } from "next";
 import "./globals.css";
 import ScrollToTop from '../components/ScrollToTop';
 
-
 export const metadata: Metadata = {
-  title: "Chotika Bangboon | Data Engineering Portfolio",
-  description: "Data Engineering portfolio showcasing experience in data pipelines, automation, and digital solutions. Passionate about the intersection of logic and creativity.",
-  keywords: ["Data Engineering", "Data Science", "Portfolio", "Chotika Bangboon", "Python", "Machine Learning", "โชติกา บางบุญ","chotika bangboon"],
-  authors: [{ name: "Chotika Bangboon" }],
+  metadataBase: new URL("https://portfolio-chotika-bangboon.vercel.app"),
+
+  title: "Chotika Bangboon (โชติกา บางบุญ) | Portfolio",
+
+  description:
+    "Portfolio of Chotika Bangboon (โชติกา บางบุญ), Data Engineer specializing in data pipelines and analytics solutions.",
+
+  keywords: [
+    "Chotika Bangboon",
+    "โชติกา บางบุญ",
+    "Data Engineering",
+    "Portfolio",
+    "Python",
+    "Machine Learning",
+	"Data Analys",
+	"Data Scientist",
+	"UX/UI",
+  ],
+
   openGraph: {
-    title: "Chotika Bangboon | Data Engineering Portfolio",
-    description: "Portfolio of Chotika Bangboon (โชติกา บางบุญ), Data Engineer specializing in data pipelines, automation, and analytics solutions.",
+    title: "Chotika Bangboon (โชติกา บางบุญ) | Portfolio",
+    description:
+      "Portfolio of Chotika Bangboon (โชติกา บางบุญ), Data Engineer specializing in data pipelines.",
     type: "website",
+    url: "https://portfolio-chotika-bangboon.vercel.app",
+  },
+
+  alternates: {
+    canonical: "/",
   },
 };
 
@@ -22,7 +42,6 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" className="scroll-smooth">
-      
       <body className="bg-neutral-50 text-neutral-900 antialiased">
         {children}
         <ScrollToTop />
@@ -35,12 +54,11 @@ export default function RootLayout({
               "@type": "Person",
               name: "Chotika Bangboon",
               alternateName: "โชติกา บางบุญ",
-              url: "https://portfolio-chotika.vercel.app/",
+              url: "https://portfolio-chotika-bangboon.vercel.app",
               jobTitle: ["Data Engineer", "Data Scientist"]
             }),
           }}
         />
-        
       </body>
     </html>
   );
